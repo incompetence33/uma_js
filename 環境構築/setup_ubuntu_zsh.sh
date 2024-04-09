fdf84f6a3a47d9b1f08c20ad06d3f385f93e0e81
incompetence33 <89658872+incompetence33@users.noreply.github.com>
2024-04-09 19:04:19 +0900

    Update setup_ubuntu_zsh.sh
    オプションや実行順の変更

--- 環境構築/setup_ubuntu_zsh.sh
+++ 環境構築/setup_ubuntu_zsh.sh
@@ -13,13 +13,10 @@ setup_ubuntu(){
 	echo "途中何回かパスワードを求められると思いますが、全てUbuntuをインストールしたときに決めたものを入力すればOKです。"
 	read -e -p "よろしければなにかキーを押してください。"
 	sudo apt update && \
-	sudo apt install bc aria2 git jq curl lame nkf language-pack-ja rename sqlite3 tar unar unzip vim zsh
+	sudo apt install bc aria2 git jq curl lame nkf language-pack-ja rename sqlite3 tar unar unzip vim zsh -y
 	mv ~/.zshrc ~/.zshrc_bak
 	curl -Lo ~/.zshrc "https://raw.githubusercontent.com/incompetence33/uma_js/master/%E7%92%B0%E5%A2%83%E6%A7%8B%E7%AF%89/.zshrc"
 	curl -Lo ~/.myfunctions "https://raw.githubusercontent.com/incompetence33/uma_js/master/%E7%92%B0%E5%A2%83%E6%A7%8B%E7%AF%89/.myfunctions"
-	echo "デフォルトのShellをBashからZshにします。"
-	echo "パスワードを入力してください。"
-	chsh -s /usr/bin/zsh
 	git clone https://github.com/zsh-users/zsh-completions.git ~/.zsh-completions
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh-syntax-highlighting
 	sudo update-locale LANG="ja_JP.UTF-8"
@@ -27,11 +24,14 @@ setup_ubuntu(){
 	yes | ~/.fzf/install 
 	curl -fsSL https://bun.sh/install | bash
 	echo "このウィンドウを閉じてもう一度WSLを起動したときZshになっていればOKです。"
-	rm ~/setup_ubuntu_zsh.sh
+	rm -f ~/setup_ubuntu_zsh.sh
 	mkdir -p ~/commands/bin
 	curl -Lo ~/vgmstream-cli.tar.gz "$(curl -sL http://vgmstream.org/ | tr '"' '\n' | \grep 'vgmstream-linux-cli.tar.gz')"
 	tar -xvf ~/vgmstream-cli.tar.gz -C ~/commands/bin
-	rm ~/vgmstream-cli.tar.gz
+	rm -f ~/vgmstream-cli.tar.gz
+ 	echo "デフォルトのShellをBashからZshにします。"
+	echo "パスワードを入力してください。"
+	chsh -s /usr/bin/zsh
 }
 
 if [[ "${DISTRIBUTION}" == *buntu ]]; then cd ~;setup_ubuntu;else echo "WSLのUbuntu用のSetUpスクリプトなのでUbuntuで実行してください。";fi
